b14b4ad0552a2b5405c83a803093d4e94112eee9
Félix Saparelli <felix@passcod.name>
2017-09-11 20:12:07 +1200

    Add soft deletes

--- app/Support/Model.php
+++ app/Support/Model.php
@@ -3,10 +3,15 @@
 namespace App\Support;
 
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Model extends EloquentModel
 {
+    use SoftDeletes;
+
     const CREATED_AT = 'created';
     const UPDATED_AT = 'updated';
+    const DELETED_AT = 'deleted';
     protected $dateFormat = 'Y-m-d H:i:s.uP';
+    protected $dates = ['deleted'];
 }
